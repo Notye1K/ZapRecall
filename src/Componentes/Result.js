9239a3ca4df028d4cc07367582c77e72ea60a117
Kl3yton <kleyton_vccamargo@hotmail.com>
2021-12-12 16:07:50 -0300

    bonus 2 pronto

--- src/Componentes/Result.js
+++ src/Componentes/Result.js
@@ -1,27 +1,34 @@
 import Party from "../assets/party.png"
 import Sad from "../assets/sad.png"
+import Next from "../assets/next.png"
+
 
 
 export default function Result (props) {
     return (
-        props.input === '' ? (
-        props.failure === 0 ? (<Screen message='winner'/>) : (<Screen message='failure'/>)) : (
-        props.input <= props.meta ? (<Screen message='winner'/>) : (<Screen message='failure'/>)
-        )
+        <div className='result'>
+            {props.input === '' ? (
+            props.failure === 0 ? (<Screen message='winner'/>) : (<Screen message='failure'/>)) : (
+            props.input <= props.meta ? (<Screen message='winner'/>) : (<Screen message='failure'/>)
+            )}
+
+            <button onClick={() => window.location.reload()} className="tryAgain">Tentar novamente <img src={Next} alt='next'/></button>
+        </div>
     )
+
 }
 
 function Screen (props){
     return (
         props.message === 'winner' ? (
-            <div className='result'>
+            <>
                 <h1>PARABÉNS! <img src={Party} alt="festa"/></h1>
                 <span> Você não esqueceu de nenhum flashcard!</span>
-            </div>) : (
-            <div className='result'>
+            </>): (
+            <>
                 <h1>Putz..  <img src={Sad} alt='Cara de triste'/></h1>
                 <span> Você esqueceu alguns flashcards..<br/>
                     Não desanime! Na próxima você consegue!</span>
-            </div>)
+            </>)
     )
 }
