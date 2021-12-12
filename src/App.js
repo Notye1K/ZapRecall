2f7586f7747f3bf4b7c78d0464521686b43b3da9
Kl3yton <kleyton_vccamargo@hotmail.com>
2021-12-12 17:07:40 -0300

    tudo pronto

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import { useState } from "react"
 export default function App(){
     const [screen, setScreen] = useState(0)
     const [input, setInput] = useState('')
+    const [data, setData] = useState([])
+    const [title, setTitle] = useState('')
+
 
     
 
     return(
     <>
-        {screen === 0 ? <Start input={input} setInput={setInput} setScreen={setScreen}/> : <FlashCard input={input}/>}
+        {screen === 0 ? <Start setTitle={setTitle} setData={setData} input={input} setInput={setInput} setScreen={setScreen}/> : <FlashCard title={title} data={data} input={input}/>}
     </>)
 }
--- src/Componentes/FlashCard.js
+++ src/Componentes/FlashCard.js
@@ -4,11 +4,7 @@ import LogoMini from "../assets/logo-mini.png"
 import { useState } from "react"
 import Result from "./Result"
 
-export default function FlashCard(props) {
-    const data = [{question:"O que é JSX?", answer:"Uma extensão de linguagem do JavaScript"}, {question:"O React é ___", answer:"uma biblioteca JavaScript para construção de interfaces"},
-    {question:"Componentes devem iniciar com ___", answer:" letra maiúscula"}, {question:"Podemos colocar __ dentro do JSX", answer:"expressões"},
-    {question:"O ReactDOM nos ajuda __", answer:"interagindo com a DOM para colocar componentes React na mesma"}, {question:"Usamos o npm para __ ", answer:"gerenciar os pacotes necessários e suas dependências"},
-    {question:"Usamos props para __", answer:"passar diferentes informações para componentes "}, {question:"Usamos estado (state) para __ ", answer:"dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}]
+export default function FlashCard({data, input, title}) {
 
     const [screenQuestion, setScreenQuestion] = useState(0)
     const [index, setIndex] = useState(0)
@@ -20,10 +16,11 @@ export default function FlashCard(props) {
     return (
         <div className='flashCard'>
             <img className='miniLogo' src ={LogoMini} alt='logo-mini'/>
+            {result === 0 && <div className='title'>{title}</div>}
             { result === 0 ? (screenQuestion === 0 ? (
             (<CardQuestion index={index} indexLength={data.length} question={data[index].question} setScreenQuestion = {setScreenQuestion}/>)
             ) : <CardAnswer meta={meta} setMeta={setMeta} setFailure={setFailure} index={index} indexLength={data.length} answer={data[index].answer} question={data[index].question} setIndex={setIndex} setScreenQuestion = {setScreenQuestion} setResult={setResult}/>
-            ) : <Result input={props.input} meta={meta} failure={failure}/>}
+            ) : <Result input={input} meta={meta} failure={failure}/>}
         </div>
     )
 }
