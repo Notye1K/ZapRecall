9724241e434f70c7d94f7aca926ac81d7dd802f7
Kl3yton <kleyton_vccamargo@hotmail.com>
2021-12-12 17:45:05 -0300

    css mobile

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import "./css/reset.css"
 import "./css/style.css"
+import "./css/mobile.css"
 
 
 ReactDOM.render(<App/>, document.querySelector('.root'))
